6658af79c61894a0e0330a976a2716772b109733
Ivan Yatskevich <ivan@yatskevich.com>
2012-12-10 00:47:19 +0300

    backup original zsh config files before creating new ones

--- setup.sh
+++ setup.sh
@@ -6,8 +6,18 @@ DOTFILES=$PWD
 # and .zshenv symlink 
 _zsh_config_dir="$DOTFILES/zsh"
 
-ln -s "$_zsh_config_dir/env" $HOME/.zshenv 
+function backup {
+  if [[ -L $1 ]]; then
+    unlink "$1"
+  elif [[ -f $1 ]]; then
+    mv "$1" "$1.orig.`date +%s`"
+  fi
+}
 
+backup "$HOME/.zshenv"
+ln -s "$_zsh_config_dir/env" "$HOME/.zshenv" 
+
+backup "$HOME/.zshrc"
 _zsh_config_dir_escaped="${_zsh_config_dir//\//\\/}"
 sed "s/__zsh_config_path__/${_zsh_config_dir_escaped}/g" $DOTFILES/templates/zshrc > $HOME/.zshrc
 
