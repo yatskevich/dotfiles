01f4f12a75b30f5c895482f2e37fcab9570bdde1
Ivan Yatskevich <ivan@yatskevich.com>
2012-12-09 23:03:12 +0300

    setup .zshrc and .zshenv

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,5 +1,15 @@
-# TODO: checkout from git
-# TODO: config symlinks
+# TODO: fetch from git
+
+DOTFILES=$PWD
+
+# create .zshrc with proper config location
+# and .zshenv symlink 
+_zsh_config_dir="$DOTFILES/zsh"
+
+ln -s "$_zsh_config_dir/env" $HOME/.zshenv 
+
+_zsh_config_dir_escaped="${_zsh_config_dir//\//\\/}"
+sed "s/__zsh_config_path__/${_zsh_config_dir_escaped}/g" $DOTFILES/templates/zshrc > $HOME/.zshrc
 
 # change default shell to zsh
 chsh -s `which zsh`
